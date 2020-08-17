f156058633badff082a4c060f75ff365d34fb8cc
danruelop <danruelop@alum.us.es>
2020-08-17 12:56:39 +0200

    Añadidos nivel de Aire y de Fuego. Faltan Enemigos.

--- Assets/Scripts/GameStuff/WaterLevelRoom.cs
+++ Assets/Scripts/GameStuff/WaterLevelRoom.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterLevelRoom : MonoBehaviour
+public class WaterLevelRoom : Room
 {
     // Start is called before the first frame update
     void Start()
